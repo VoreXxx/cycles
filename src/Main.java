3cdbfbebd5071246fa157a4dad20fd5e1f71ce39
VoreXxx <vorex.00@mail.ru>
2023-09-28 22:29:14 +0600

    First commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 2020; i < 2070; i = i + 4) {
+            System.out.println("Вискосный год " + i);
+        }
+    }
+}
